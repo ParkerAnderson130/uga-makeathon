e28b2c9b7097c37b87ad8d5696540e733707054c
Parker <parkeranderson130@gmail.com>
2022-10-02 12:11:34 -0400

    Added meditation cards

--- src/pages/meditation.js
+++ src/pages/meditation.js
@@ -4,7 +4,9 @@ import '../static/css/meditation.css';
 const  Meditation = () =>{
   return (
     <div class="music">
-      <div class="music-selection"><h2>Rain Noises</h2></div>
+      <div class="music-selection" id="ocean"><h2>Ocean</h2></div>
+      <div class="music-selection" id="rain"><h2>Rain</h2></div>
+      <div class="music-selection" id="under"><h2>Underwater</h2></div>
     </div>
   );
 }
