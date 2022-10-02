39d15cfebf53cc271769a1408d37792b8a3dd664
Parker <parkeranderson130@gmail.com>
2022-10-02 14:34:40 -0400

    Added audio (kind of working)

--- src/pages/meditation.js
+++ src/pages/meditation.js
@@ -1,12 +1,33 @@
 import React from 'react';
+//import { useState } from "react";
 import '../App.css'
 import '../static/css/meditation.css';
-const  Meditation = () =>{
+
+const  Meditation = () => {
+  const audio = new Audio(require('../static/assets/ocean.mp3'));
+  let isPlaying = false;
+
+  const player = () => {
+    if(isPlaying){
+      audio.pause();
+      isPlaying = false;
+    } else{
+      audio.play();
+      isPlaying = true;
+    }
+  }
+
   return (
     <div class="music">
-      <div class="music-selection" id="ocean"><h2>Ocean</h2><button>Play</button></div>
-      <div class="music-selection" id="rain"><h2>Rain</h2></div>
-      <div class="music-selection" id="under"><h2>Underwater</h2></div>
+      <div class="music-selection" id="rain"><h2>Rain</h2>
+        <button class="play" onClick={player}>&#9654;</button>
+      </div>
+      <div class="music-selection" id="waves"><h2>Waves</h2>
+        <button class="play" onClick={player}>&#9654;</button>
+      </div>
+      <div class="music-selection" id="under"><h2>Underwater</h2>
+        <button class="play" onClick={player}>&#9654;</button>
+      </div>
     </div>
   );
 }
