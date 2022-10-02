d77542c1bfa5113f28948ef20157ac0b7dfc9d88
Parker <parkeranderson130@gmail.com>
2022-10-02 15:55:11 -0400

    Merging

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,11 +2,14 @@ import React from 'react';
 import '../App.css'
 import '../static/css/navbar.css';
 import { Link } from "react-router-dom";
+import pearl from '../static/assets/pearl.png'
 
 const Navbar= () =>{ 
     return (
         <ul className="nav">
-            <h1><Link to="/">Flow</Link></h1>
+            <div><h1><Link to="/">Flow</Link></h1>
+                <div><img src={pearl} alt="Pearls"/><p>x 18</p></div>
+            </div>
             <div>
                 <button><Link to="/timer">Timer</Link></button>
                 <button><Link to="/meditate">Meditation</Link></button>
