a27a49b1f3e0f7479ef09fa609a8a007129c6286
Parker <parkeranderson130@gmail.com>
2022-10-02 12:16:37 -0400

    Made navbar styles non-universal

--- src/pages/meditation.js
+++ src/pages/meditation.js
@@ -4,7 +4,7 @@ import '../static/css/meditation.css';
 const  Meditation = () =>{
   return (
     <div class="music">
-      <div class="music-selection" id="ocean"><h2>Ocean</h2></div>
+      <div class="music-selection" id="ocean"><h2>Ocean</h2><button>Play</button></div>
       <div class="music-selection" id="rain"><h2>Rain</h2></div>
       <div class="music-selection" id="under"><h2>Underwater</h2></div>
     </div>
