bbcbfecfa4ad9df8a975550c202f08aef97aae22
Alistair <alistairzlee@gmail.com>
2022-10-02 16:07:40 -0400

    updated slides information and css

--- src/components/SliderData.js
+++ src/components/SliderData.js
@@ -1,23 +1,23 @@
 export const SliderData = [
     {
-        image: 'https://images.unsplash.com/photo-1488188840666-e2308741a62f',
-        title: "General Information",
-        content: "Test"
+        image: 'https://i.imgur.com/azJKY6D.jpg',
+        title: "How To Use",
+        content: ""
     },
     {
-        image: 'https://images.unsplash.com/photo-1484291470158-b8f8d608850d',
+        image: 'https://i.imgur.com/hUjWw0R.jpg',
         title: "Charities",
-        content: "Test"
+        content: ""
     },
     {
-        image: 'https://images.unsplash.com/photo-1468581264429-2548ef9eb732',
+        image: 'https://i.imgur.com/SZfFVOX.jpeg',
         title: "Sponsors",
-        content: "Test"
+        content: ""
     },
     {
-        image: 'https://images.unsplash.com/photo-1551244072-5d12893278ab',
-        title: "About Us",
-        content: "Test"
+        image: 'https://i.imgur.com/k8wqagg.jpg',
+        title: "Credits",
+        content: ""
     }
 
 ]
